fe9b372ae8045173202fad96e349880622ba5bee
Youssef <yousseferrachid@gmail.com>
2026-08-03 22:51:10 +0100

    Refactor controllers to use DTOs and MapStruct

--- backend/src/main/java/com/logitrack/logitrack/controller/OrderController.java
+++ backend/src/main/java/com/logitrack/logitrack/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.logitrack.logitrack.controller;
 
+import com.logitrack.logitrack.dto.request.OrderRequest;
+import com.logitrack.logitrack.dto.response.OrderResponse;
+import com.logitrack.logitrack.mapper.OrderMapper;
 import com.logitrack.logitrack.model.*;
 import com.logitrack.logitrack.service.OrderService;
 import org.springframework.http.ResponseEntity;
@@ -11,36 +14,44 @@ import java.util.List;
 public class OrderController {
 
     private final OrderService orderService;
+    private  final OrderMapper orderMapper;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, OrderMapper orderMapper) {
         this.orderService = orderService;
+        this.orderMapper = orderMapper;
     }
 
     @PostMapping
-    public ResponseEntity<Order> createOrder(@RequestParam Long clientId) {
-        return ResponseEntity.ok(orderService.createOrder(clientId));
+    public ResponseEntity<OrderResponse> createOrder(@RequestParam OrderRequest request) {
+        Order order = orderMapper.toEntity(request);
+        Order savedOrder = orderService.createOrder(order.getClient().getId());
+        return ResponseEntity.ok(orderMapper.toResponse(savedOrder));
     }
 
     @GetMapping
-    public ResponseEntity<List<Order>> getAllOrders() {
-        return ResponseEntity.ok(orderService.getAllOrders());
+    public ResponseEntity<List<OrderResponse>> getAllOrders() {
+        List<Order> orders = orderService.getAllOrders();
+        return ResponseEntity.ok(orderMapper.toResponseList(orders));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
-        return ResponseEntity.ok(orderService.getOrderById(id));
+    public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long id) {
+        Order order = orderService.getOrderById(id);
+        return ResponseEntity.ok(orderMapper.toResponse(order));
     }
 
     @PutMapping("/{id}/status")
-    public ResponseEntity<Order> updateStatus(
+    public ResponseEntity<OrderResponse> updateStatus(
             @PathVariable Long id,
             @RequestParam OrderStatus status) {
-        return ResponseEntity.ok(orderService.updateOrderStatus(id, status));
+        Order updateOrder = orderService.updateOrderStatus(id,status);
+        return ResponseEntity.ok(orderMapper.toResponse(updateOrder));
     }
 
     @GetMapping("/client/{clientId}")
-    public ResponseEntity<List<Order>> getOrdersByClient(@PathVariable Long clientId) {
-        return ResponseEntity.ok(orderService.getOrdersByClient(clientId));
+    public ResponseEntity<List<OrderResponse>> getOrdersByClient(@PathVariable Long clientId) {
+        List<Order> orders = orderService.getOrdersByClient(clientId);
+        return ResponseEntity.ok(orderMapper.toResponseList(orders));
     }
 
     @GetMapping("/count")
