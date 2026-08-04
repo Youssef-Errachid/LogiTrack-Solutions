32dcf5741bae89e23ef18f63d8e179f9607a946b
Youssef <yousseferrachid@gmail.com>
2026-08-04 16:02:55 +0100

    implement authentication service

---
+++ backend/src/main/java/com/logitrack/logitrack/service/AuthService.java
@@ -0,0 +1,63 @@
+package com.logitrack.logitrack.service;
+
+import com.logitrack.logitrack.dto.request.LoginRequest;
+import com.logitrack.logitrack.dto.request.RegisterRequest;
+import com.logitrack.logitrack.dto.response.AuthResponse;
+import com.logitrack.logitrack.model.User;
+import com.logitrack.logitrack.repository.UserRepository;
+import com.logitrack.logitrack.security.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        User savedUser =  userRepository.save(user);
+        String token = jwtService.generateToken(savedUser);
+
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse login(LoginRequest dto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getEmail(),
+                        dto.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String token = jwtService.generateToken(user);
+
+        return AuthResponse.builder()
+                .token(token)
+                .username(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .build();
+    }
+}
