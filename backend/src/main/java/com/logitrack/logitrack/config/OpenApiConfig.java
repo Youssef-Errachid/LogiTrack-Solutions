740ba5236ad0b876b4803003ea04d3590c4ac196
Youssef <yousseferrachid@gmail.com>
2026-08-04 19:18:06 +0100

    add OpenAPI configuration with JWT bearer authentication

---
+++ backend/src/main/java/com/logitrack/logitrack/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.logitrack.logitrack.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "LogiTrack Solution", version = "part-2"),
+        security = @SecurityRequirement(name = "bearerAuth"),
+        tags = {
+                @Tag(name = "Authentication", description = "User login and registration")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
