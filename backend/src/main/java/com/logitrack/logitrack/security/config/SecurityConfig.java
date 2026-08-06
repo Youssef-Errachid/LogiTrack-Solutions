a44ce2a65add17d7d7b9777ca671660845e0e5df
Youssef <yousseferrachid@gmail.com>
2026-08-06 22:50:17 +0100

    fix(security): update CORS configuration for frontend development

--- backend/src/main/java/com/logitrack/logitrack/security/config/SecurityConfig.java
+++ backend/src/main/java/com/logitrack/logitrack/security/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174"));
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
