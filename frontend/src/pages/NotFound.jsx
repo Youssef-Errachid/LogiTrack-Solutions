4d28391d8ea5093c7ed4c13eae1b2c3ca774fea6
Youssef <yousseferrachid@gmail.com>
2026-08-08 21:33:13 +0100

    add simple 404 not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,29 @@
+import { Box, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
-    return <h1>Not Found Page</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      textAlign="center"
+    >
+      <Typography variant="h1" fontWeight="bold" color="primary">
+        404
+      </Typography>
+
+      <Typography variant="h6" mb={3}>
+        Page Not Found
+      </Typography>
+
+      <Button variant="contained" onClick={() => navigate("/dashboard")}>
+        Back to Dashboard
+      </Button>
+    </Box>
+  );
 }
